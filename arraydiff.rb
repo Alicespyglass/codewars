52f1880e25c1b202275bbc43c59c2e9322b14fa7
alicespyglass <alicespyglass@gmail.com>
2017-03-04 23:59:04 +0000

    ArrayDiff - kyu6 - solution

---
+++ arraydiff.rb
@@ -0,0 +1,8 @@
+# Your goal in this kata is to implement an difference function
+# which subtracts one list from another.
+def array_diff(a, b)
+# It should remove all values from list a, which are present in list b.
+  a - b
+# If a value is present in b
+# all of its occurrences must be removed from the other:
+end
